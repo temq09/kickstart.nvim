b14a033425732ef8e2b60f13ed63021a73e67272
Artem Ushakov <hello@temq.dev>
2026-08-24 22:52:07 +0100

    add a hotkey for no neck pain

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,5 +1,5 @@
-vim.pack.add({
-    'https://github.com/nvim-tree/nvim-web-devicons',
-    'https://github.com/nvim-lualine/lualine.nvim'
-})
+vim.pack.add {
+  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/nvim-lualine/lualine.nvim',
+}
 require('lualine').setup()
--- lua/custom/plugins/no_neck_pain.lua
+++ lua/custom/plugins/no_neck_pain.lua
@@ -1,2 +1,6 @@
-vim.pack.add({'https://github.com/shortcuts/no-neck-pain.nvim'})
-require('no-neck-pain').setup()
+vim.pack.add { 'https://github.com/shortcuts/no-neck-pain.nvim' }
+require('no-neck-pain').setup {
+  width = 130,
+}
+
+vim.keymap.set('n', '<leader>np', function() require('no-neck-pain').toggle() end, { desc = 'Toggle No Neck Pain' })
